c363bb23442aed0bcc1747c3e5dd1fbb6b47b2c8
Nothile Moyo <Nothile1@gmail.com>
2026-05-11 00:22:48 +0100

    Frequency counter is now complete

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Data Structures and Algorithms in TypeScript
 
 > Frequency Counter
+
+To test a file, run the command
+
+```
+npx tsx <filename>
+```
+
+**Note: This is because this project is built in TypeScript and must be parsed for results**
--- src/patterns/frequency-counter.ts
+++ src/patterns/frequency-counter.ts
@@ -9,19 +9,22 @@
  * It does so by iterating through a list, and then proceeding to count the frequency of each node in a hashmap
  */
 
-// ==================================================================================
+// =================================================================================================
 // Anagram
 //
 // An anagram is a word or phrase that can have the letters re-arranged
-// ==================================================================================
-
+// =================================================================================================
 type ANAGRAM = {
-    method: (firstString: string, secondString: string) => boolean
+    method: (firstInput: string, secondInput: string) => boolean
 };
 
-const isAnagram: ANAGRAM['method'] = (firstString, secondString) => {
+type FREQUENCY_COUNTER = {
+    method: (firstInput: number[], secondInput: number[]) => boolean
+};
 
-    if (firstString.length !== secondString.length) {
+const isAnagram: ANAGRAM['method'] = (firstInput, secondInput) => {
+
+    if (firstInput.length !== secondInput.length) {
         return false;
     }
 
@@ -31,17 +34,66 @@ const isAnagram: ANAGRAM['method'] = (firstString, secondString) => {
     const firstCharCounter: Record<string, number> = {};
     const secondCharCounter: Record<string, number> = {};
 
-    for (const char of firstString.toLowerCase()) {
+    for (const char of firstInput.toLowerCase()) {
         firstCharCounter[char] = (firstCharCounter[char] || 0) + 1;
     }
 
-    for (const char of secondString.toLowerCase()) {
+    for (const char of secondInput.toLowerCase()) {
         secondCharCounter[char] = (secondCharCounter[char] || 0) + 1;
     }
 
     // Checks that the frequencies are the same, otherwise, the function fails
     for (const char in firstCharCounter) {
         if (firstCharCounter[char] !== secondCharCounter[char]) {
+            success = false;
+        }
+    }
+
+    return success;
+};
+
+// =================================================================================================
+// Frequency counter
+// 
+// For this, we must check that the values in both arrays would be the same if we squared the values of the first array
+// E.G [1,2] would be equal to [1, 4] in the comparison
+// 
+// This approach checks the frequency of items in two arrays and converts them to hashmap
+// It does this to keep the time complexity to O(n) instead of O(n logn) or O(n^2)
+// =================================================================================================
+const firstInput = [1, 2, 3]; // True 
+const secondInput = [1, 2, 3]; // False 
+const thirdInput = [1, 2, 1]; // False
+
+const firstComparison = [4, 1, 9];
+const secondComparison = [1, 9];
+const thirdComparison = [4, 4, 1];
+
+const frequencyCounter: FREQUENCY_COUNTER['method'] = (firstInput, secondInput) => {
+
+    // Only compare values that are the same, otherwise, return false
+    if (firstInput.length !== secondInput.length) {
+        return false;
+    }
+
+    // Square the first input for comparison as that's the task
+    const firstInputSquared = firstInput.map((number) => number *= number);
+    let success = true;
+    let firstNumberCounter: Record<string, number> = {};
+    let secondNumberCounter: Record<string, number> = {};
+
+    // Check the char as a key of the string, then increment the number (counter) so we can compare them later
+    for (const value of firstInputSquared) {
+        // If the string at char [char] doesn't have a value, set it to 1, otherwise, increment it
+        firstNumberCounter[value] = (firstNumberCounter[value] || 0) + 1;
+    }
+
+    for (const value of secondInput) {
+        secondNumberCounter[value] = (secondNumberCounter[value] || 0) + 1;
+    }
+
+    for (const value in firstNumberCounter) {
+        if (firstNumberCounter[value] !== secondNumberCounter[value]) {
             success = false;
         }
     }
@@ -60,3 +112,8 @@ console.log(isAnagram("qwerty", "qeywrt")); // True
 console.log(isAnagram("texttwisttime", "timetwisttext")); // True
 
 console.log("\n\n");
+
+console.log("Frequency counter:");
+console.log(frequencyCounter(firstInput, firstComparison)); // True
+console.log(frequencyCounter(secondInput, secondComparison)); // False
+console.log(frequencyCounter(thirdInput, thirdComparison)); // False
