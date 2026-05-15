e261bd11f00e157d096e810e782c0a88cdcc9adf
Nothile Moyo <Nothile1@gmail.com>
2026-05-15 23:00:18 +0100

    Multiple pointers exercise has been done for counting unique values.
    I'll submit this and check out the result

--- src/patterns/multiple-pointers.ts
+++ src/patterns/multiple-pointers.ts
@@ -55,10 +55,57 @@ const countUniqueValues = (values: number[]) : number => {
     return uniqueValues;
 };
 
+// =======================================================================================================================
+// Multiple pointers
+//
+// This challenge is to use the current array and update it to get the unique values in that
+// Then we can use the index position and return it
+// =======================================================================================================================
+const multiplePointers = (values: number[]) : number => {
+
+    if (values.length === 0) {
+        return 0;
+    }
+
+    if (values.length === 1) {
+        return 1;
+    }
+
+    const max = values.length;
+    let firstPointer = 0;
+
+    for (let index = 1; index < max; index++) {
+
+        // If the second value is different from the first, move forward
+        if (values[firstPointer] !== values[index]) {
+            firstPointer++;
+
+            // We need to make sure this value exists if we're to set it
+            const value = values[index];
+            if (value !== undefined) {
+                values[firstPointer] = value;
+            }
+        }
+    }
+
+    // Increment based on index position
+    return firstPointer += 1;
+};
+
 // Outputs
+/*
 console.log("Count unique values");
 console.log(countUniqueValues([1, 1, 1, 1, 1, 2])); // 2
 console.log(countUniqueValues([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13])); // 7
 console.log(countUniqueValues([])); // 0
 console.log(countUniqueValues([-2, -1, -1, 0, 1])); // 4
+*/
+
+console.log("\n\n");
+
+console.log("Multiple pointers");
+console.log(multiplePointers([1, 1, 1, 1, 1, 2])); // 2
+console.log(multiplePointers([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13])); // 7
+console.log(multiplePointers([])); // 0
+console.log(multiplePointers([-2, -1, -1, 0, 1])); // 4
 
