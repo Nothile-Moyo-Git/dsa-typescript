b0504a6d2b82af16cbb39876939de581256dbdd3
Nothile Moyo <Nothile1@gmail.com>
2026-06-22 21:41:10 +0100

    I've done the recursion example for sumRange

--- src/patterns/recursion.ts
+++ src/patterns/recursion.ts
@@ -7,3 +7,22 @@
   * 
   * Date created: 19/06/2026
   */
+
+// =======================================================================================================================
+// sumRange
+//
+// Counts down values until it hits 1, then returns the sum
+// It adds the value to each number it reduces as it moves towards to the sum
+//
+// Time complexity: O(log n)
+// =======================================================================================================================
+const sumRange = (num: number) : number => {
+    if (num === 1) return 1;
+    return num + sumRange(num - 1); 
+};
+
+console.log("Sum range: ");
+console.log("For a value of 1: ", sumRange(1));
+console.log("For a value of 2: ", sumRange(2));
+console.log("For a value of 3: ", sumRange(3));
+console.log("For a value of 4: ", sumRange(4));
