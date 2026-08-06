0d259639b9abb5f597d141246e490a306100c97c
Nothile Moyo <Nothile1@gmail.com>
2026-08-06 21:36:15 +0100

    Started making progress on binary search which is basically divide and conquer.
    Linear search worked just fine, I think this pattern has already been covered in optional challenges but we'll see

---
+++ src/searches/binary-search.ts
@@ -0,0 +1,21 @@
+/**
+  * Name: Binary Search
+  * 
+  * Description: Performs a search that iterates through the data object once, but it breaks it down.
+  * It uses the divide and conquer 
+  * 
+  * Date created: 06/08/2026
+ */
+
+// =======================================================================================================================
+// binarySearch
+//
+// Iterate through an object and convert all the strings inside it into an array
+// =======================================================================================================================
+const binarySearch = (numbers: number[], value: number): number => {
+
+    
+
+    return -1;
+};
+
---
+++ src/searches/linear-search.ts
@@ -0,0 +1,35 @@
+/**
+  * Name: Linear Search
+  * 
+  * Description: Performs a simple search that iterates through the data object once
+  * 
+  * Date created: 06/08/2026
+ */
+
+// =======================================================================================================================
+// linearSearch
+//
+// Iterate through an object and convert all the strings inside it into an array
+// =======================================================================================================================
+/* const linearSearch = (numbers: number[], value: number): number => {
+
+  const size = numbers.length;
+
+  for (let index = 0; index < size; index++) {
+    const number = numbers[index];
+
+    // Return the index if we find it
+    if (value === number) {
+      return index;
+    }
+  };
+
+  return -1;
+}; */
+
+// console.log(linearSearch([10, 15, 20, 25, 30], 15)); // 1
+// console.log(linearSearch([9, 8, 7, 6, 5, 4, 3, 2, 1, 0], 4)); // 5
+// console.log(linearSearch([100], 100)); // 0
+// console.log(linearSearch([1,2,3,4,5], 6)); // -1
+// console.log(linearSearch([9, 8, 7, 6, 5, 4, 3, 2, 1, 0], 10)); // -1
+// console.log(linearSearch([100], 200)); // -1
