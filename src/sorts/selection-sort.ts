5670f9be5bda9042710f9615b21bfcd8a534dd18
Nothile Moyo <Nothile1@gmail.com>
2026-08-31 21:36:08 +0100

    Made some progress on selection sort, looks like the swap algorithm is wrong as the numbers end up quite distorted, but, we do as we do

--- src/sorts/selection-sort.ts
+++ src/sorts/selection-sort.ts
@@ -3,7 +3,58 @@
  * 
  * Description: Another way to sort algorithms, find the smallest number in an array
  * 
- * Base case is likely there being no swaps post iteration. The smallest values moves to the bottom
+ * Selection sort starts with the initial value, then compares the values and stores the index of the lowest value
+ * At the end of the loop, reassign temporary to the next number and continue
  * 
  * Date created: 30/08/2026
  */
+
+// =======================================================================================================================
+// Selection Sort
+//
+// Receive an array of numbers
+// Set a value to temp (initial) and compare it to other numbers
+// Keep the index of the smallest value
+// Move the value left
+// 
+// Time complexity: O(n ^ 2) as it needs to iterate squared, worst case
+// =======================================================================================================================
+
+type sort = {
+    method: (values: number[]) => number[] 
+}
+
+const selectionSort: sort['method'] = (values) => {
+
+    let result: number[] = [];
+    const size = values.length;
+
+    for (let i = 0; i < size; i++) {
+
+        let smallest = i;
+
+        for (let j = 0; j < size; j++) {
+
+            let temp = values[smallest];
+            const value = values[j];
+
+            console.log("Temp: ", temp);
+            console.log("Value: ", value);
+
+            if (temp !== undefined && value !== undefined) {
+                if (temp > value) {
+                    result[smallest] = value;
+                    result[j] =  temp;
+                }
+            }
+
+            console.log("\n");
+        }
+    }
+
+    return result;
+};
+
+const array1 = [4, 20, 12, 10, 7, 9];
+console.log("Selection Sort: ");
+console.log("Selection sort: ", array1, selectionSort(array1)); // [4, 7, 9, 10, 12, 20]
