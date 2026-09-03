2c4a5ff092df4460be5f83a79d873e42660f6281
Nothile Moyo <Nothile1@gmail.com>
2026-09-03 22:56:23 +0100

    Progress with selection sort to work with strings and objects

--- src/sorts/selection-sort.ts
+++ src/sorts/selection-sort.ts
@@ -23,9 +23,22 @@
 type sort = {
     params: number[] | string[];
     method: (values: number[]| string[]) => sort['params'];
+    compare: (a: {name: string, age: number} | string, b: {name: string, age: number} | string) => number;
 }
 
-const selectionSort: sort['method'] = (values) => {
+const comparison: sort['compare'] = (a, b) => {
+
+    if (typeof(a) === 'object' && typeof(b) === 'object') {
+        return b.age - a.age;
+    }
+
+    if (a < b) { return -1;}
+    else if (a > b) { return 1;}
+    return 0;
+
+};
+
+const selectionSort: sort['method'] = (values, compare?: sort['compare']) => {
 
     console.log("Original array: ", values);
 
@@ -38,6 +51,7 @@ const selectionSort: sort['method'] = (values) => {
         console.log("Current array: ", result);
         let smallest = i;
         let temp = values[smallest];
+        let functionCalled = false;
 
         // Start here as a sorted value doesn't need to be evaluated and we gain performance
         for (let j = i; j < size; j++) {
@@ -66,10 +80,21 @@ const selectionSort: sort['method'] = (values) => {
 
         const smallestValue = result[smallest];
         const originalValue = result[i];
+        let outcome = 0;
+
+        // If we have the function being passed through, then we call it and process the value
+        if (compare && smallestValue !== undefined && originalValue !== undefined) {
+
+            if (typeof(smallestValue) === 'string' && typeof(originalValue) === 'string') {
+                outcome = compare(smallestValue, originalValue);
+                console.log("Outcome: ", outcome);
+                functionCalled = true;
+            }
+        }
 
         // Do the swap here, we swap the smallest value with i if it's able to do so
         // This way, we find the smallest value, and then replace the value at position i with it
-        if (smallest > i && smallestValue !== undefined && originalValue !== undefined) {
+        if (smallest > i && smallestValue !== undefined && originalValue !== undefined && functionCalled === false) {
             result[i] = smallestValue;
             result[smallest] =  originalValue;
         }
@@ -86,6 +111,22 @@ const array3 = [1, 2, 3];
 const array4: number[] = [];
 const array5 = [4, 3, 5, 3, 43, 232, 4, 34, 232, 32, 4, 35, 34, 23, 2, 453, 546, 75, 67, 4342, 32];
 const kitties = ["LilBub", "Garfield", "Heathcliff", "Blue", "Grumpy"];
+const moarKittyData = [{
+  name: "LilBub",
+  age: 7
+}, {
+  name: "Garfield",
+  age: 40
+}, {
+  name: "Heathcliff",
+  age: 45
+}, {
+  name: "Blue",
+  age: 1
+}, {
+  name: "Grumpy",
+  age: 6
+}];
 
 console.log("Selection Sort: ");
 // console.log("Selection sort: [4, 20, 12, 10, 7, 9] ", array1, selectionSort(array1)); // [4, 7, 9, 10, 12, 20]
@@ -93,4 +134,5 @@ console.log("Selection Sort: ");
 // console.log("Selection sort: [1, 2, 3] ", selectionSort(array3)); // [1, 2, 3]
 // console.log("Selection sort: [] ", selectionSort(array4)); // []
 // console.log("Selection sort: [4, 3, 5, 3, 43, 232, 4, 34, 232, 32, 4, 35, 34, 23, 2, 453, 546, 75, 67, 4342, 32] ", selectionSort(array5)); // [2, 3, 3, 4, 4, 4, 5, 23, 32, 32, 34, 34, 35, 43, 67, 75, 232, 232, 453, 546, 4342]
-console.log('Selection sort: ["LilBub", "Garfield", "Heathcliff", "Blue", "Grumpy"]', selectionSort(kitties));
+// console.log('Selection sort: ["LilBub", "Garfield", "Heathcliff", "Blue", "Grumpy"]', selectionSort(kitties));
+console.log(`Selection sort: ${moarKittyData} `, comparison);
